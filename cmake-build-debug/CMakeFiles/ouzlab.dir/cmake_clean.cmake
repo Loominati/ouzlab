beacdfcdd35df4478a4f7cf2a5638c5b30d74651
damian <229437@edu.p.lodz.pl>
2021-02-01 19:27:42 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ouzlab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ouzlab.dir/main.cpp.obj"
+  "ouzlab.exe"
+  "ouzlab.exe.manifest"
+  "ouzlab.lib"
+  "ouzlab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ouzlab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ouzlab.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ouzlab.
+# This may be replaced when dependencies are built.
